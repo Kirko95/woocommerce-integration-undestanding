ed50a84c841891b0af52af0eeb6ed8111b8ee08f
Kirko <John.muriu95@gmail.com>
2019-02-15 10:02:56 +0300

    Create config.js

---
+++ Api/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Constants = {
+    URL: {
+        root: 'https://api-url.com/'
+    },
+    Keys: {
+        ConsumerKey: 'CONSUMER_KEY',
+	    ConsumerSecret: 'CONSUMER_SECRET'
+    }
+}
+
+module.exports = Constants;
